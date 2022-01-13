59327ec3761fb88692d9783d8c49ec5c7bb17be1
zaria <mikeburrows8114@gmail.com>
2022-01-13 12:05:40 -0500

    initial work

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+//@ts-check
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// @ts-ignore
+var usersRouter = require('./routes/api/user');
+var blogsRouter = require('./routes/api/blog');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var blogRouter = require('./routes/users');
+
+const db = require('./models');
+
+const models = require('./models')
 
 var app = express();
+
+// inside app.js
+
+// ... rest of express setup/middleware
+// ...rest of express routes, etc.
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +29,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// further down, use the model
+// db.User.create({
+//     // data
+//   })  
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/blogs', blogRouter);
+
+
+
+
+app.get('/api/v1/blogs', (req, res) => {
+    models.User.findAll().then(Blogs => {
+        res.json(Blogs)
+    })
+})
+
 
 module.exports = app;
