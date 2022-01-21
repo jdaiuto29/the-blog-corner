13e422942176d70b6ba9e24e59ac7369aa070b83
Kayci Buchanan <92894323+KayKay2000@users.noreply.github.com>
2022-01-21 16:04:35 -0500

    Shows local time and date on home page
    Signed-off-by: Kayci Buchanan <92894323+KayKay2000@users.noreply.github.com>

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -26,7 +26,7 @@ axios.get('/api/v1/blogs')
 //     }).join('')
 //     document.querySelector('#putPosts').innerHTML = html
 //     }
-    
+
 // axios.get(`/api/v1/blogs/${id}`)
 //         .then(res => {
 //             renderPosts(res.data)
@@ -34,32 +34,37 @@ axios.get('/api/v1/blogs')
 
 //second attempt
 function renderPosts(posts) {
-    const html = posts.map(post => {
-            return `<div class = "singlePost" >
-            <h4 id="blogTitle">${post.Blog.title}</h4>
-          <div class = "profilePicture" ><img onerror='this.src="pictures/no-image.jpeg"' src="${post.User.profilePicture}" height="45px" width="45px"></div>
-        <div class = "titlePost">${post.User.email.substring(0, post.User.email.indexOf('@'))}  (${post.createdAt.replace('T', ' @ ').slice(0,18)})</div>
-        <div> ${post.text}</div>
-        <input type="image" src="pictures/like.jpeg" class="likeButton" data-postId="${post.id}" width="25" height="25"> ${post.Likes.length}&emsp;
-        <input type="image" src="pictures/dislike.jpeg" class="dislikeButton" data-postId="${post.id}" width="25" height="25"> ${post.Dislikes.length}
-    </div>`
-          }).join('')
-          //figure out where to put info
-        document.querySelector('.blogPosts').innerHTML = html
-  }
-
-  axios.get('/api/v1/posts')
-    .then(res => {
-        renderPosts(res.data)
+    const userDetails = posts.map(post => {
+        if (!post.UserId) {
+            return Promise.resolve(post)
+        }
+        return axios.get(`/api/v1/users/${post.UserId}/profile`)
+            .then(user => {
+                return {
+                    ...post,
+                    user: user.data
+                }
+            })
     })
+    Promise.all(userDetails)
+        .then(posts => {
+            const html = posts.map(post => {
+                return `<div class = "singlePost" >
+                <h4 id="blogTitle">${post.Blog.title}</h4>
+            <div class = "profilePicture" ><img onerror='this.src="pictures/no-image.jpeg"' src="${post.User.profilePicture}" height="45px" width="45px"></div>
+            <div class = "titlePost">${post.user.email.substring(0, post.user.email.indexOf('@'))}  (${new Date(post.createdAt).toLocaleString()})</div>
+            <div> ${post.text}</div>
+            <input type="image" src="pictures/like.jpeg" class="likeButton" data-postId="${post.id}" width="25" height="25"> ${post.Likes.length}&emsp;
+            <input type="image" src="pictures/dislike.jpeg" class="dislikeButton" data-postId="${post.id}" width="25" height="25"> ${post.Dislikes.length}
+        </div>`
+            }).join('')
+            //figure out where to put info
+            document.querySelector('.blogPosts').innerHTML = html
+        })
+    }
 
-  
-//   axios.get(`/api/v1/blogs/${blogs.id}`)
-//     .then(res => {
-//       renderBlogs(res.data)
-//     })
+    axios.get('/api/v1/posts')
+        .then(res => {
+            renderPosts(res.data)
+        })
 
-//   axios.get(`/api/v1/blogs/${id}/posts`)
-//     .then(res => {
-//       renderPosts(res.data)
-//     })
