acf23614b55082363a0ddf648143bdebfdc9756e
Kayci Buchanan <92894323+KayKay2000@users.noreply.github.com>
2022-01-25 10:41:50 -0500

    fixed seeders. change ID numbers to fit ur tables
    Signed-off-by: Kayci Buchanan <92894323+KayKay2000@users.noreply.github.com>

--- seeders/20220116191744-add-comment-data.js
+++ seeders/20220116191744-add-comment-data.js
@@ -3,6 +3,9 @@
 module.exports = {
   up: async(queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Comments', [{
+      UserId: 1,
+      PostId: 1,
+      BlogId: 53,
       comment: 'this is a comment',
       createdAt: new Date(),
       updatedAt: new Date()
